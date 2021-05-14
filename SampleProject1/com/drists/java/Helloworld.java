249857761b31555217916cbb0dbbf165e55a7e4e
Sonali <Sonali@LAPTOP-O4AS6IEL>
2021-05-14 19:07:42 +0530

    commit for second time

--- SampleProject1/com/drists/java/Helloworld.java
+++ SampleProject1/com/drists/java/Helloworld.java
@@ -4,5 +4,6 @@ public class Helloworld {
 public static void main (String args[]) {
 	System.out.println("Hello world");
 	System.out.println("Hello world Test");
+	System.out.println("Hello world New Test");
 }
 }
