c8ccaa7372ce6d43fbca4d3695044e936ab07acf
sadadure123 <84184305+sadadure123@users.noreply.github.com>
2021-05-14 17:35:52 +0530

    New file added
    First trial

---
+++ test2.java
@@ -0,0 +1,11 @@
+class test2 {
+static public void main(String[] _A_V) {
+                String $ = "";
+		for(int x=0; ++x < _A_V.length; ) 
+
+
+			$ += _A_V[x];
+			
+		System.out.println( $ );
+	}
+}
